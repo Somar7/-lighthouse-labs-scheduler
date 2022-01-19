300ed18bf02c8658ce95c94e8265ec259290ecda
Suleyman Omar <93765065+Somar7@users.noreply.github.com>
2022-01-18 18:14:29 -0700

    Building a Button, Refactoring our Button, Building the DayListItem, Styling the DayListItem.

--- stories/index.js
+++ stories/index.js
@@ -2,9 +2,8 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
+import DayListItem from "components/DayListItem";
 import "index.scss";
-
 import Button from "components/Button";
 
 storiesOf("Button", module)
@@ -13,7 +12,7 @@ storiesOf("Button", module)
   })
   .add("Base", () => <Button>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Danger", () => <button class="button button--danger">Cancel</button>)
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
@@ -21,4 +20,15 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+  storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));

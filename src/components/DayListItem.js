d68f6297a8a53cb1189bce55fbf134d16b3ed988
Suleyman Omar <93765065+Somar7@users.noreply.github.com>
2022-01-18 19:29:20 -0700

    Building the DayList

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,7 +5,7 @@ var classNames = require('classnames');
 
 
 export default function DayListItem(props) {
-  var dayClass = classNames('day-list__item',{
+  const dayClass = classNames('day-list__item',{
 
     
     'day-list__item--selected': props.selected,
@@ -13,10 +13,20 @@ export default function DayListItem(props) {
   }
     );
 
+    const formatSpots = () => {
+      if (props.spots === 0) {
+        return "no spots remaining";
+      } else if (props.spots === 1) {
+        return "1 spot remaining";
+      } else {
+        return `${props.spots} spots remaining`;
+      }
+    };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
-  )
-}
+  )}
+
